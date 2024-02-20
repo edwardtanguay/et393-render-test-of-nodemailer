60637bae61b6fe63e74feb7dc1a623c48e14f41c
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-21 00:26:13 +0100

    sends email unprotected

--- src/routers/employeeRouter.ts
+++ src/routers/employeeRouter.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Router } from "express";
 import * as employeeHandlers from "../handlers/employeeHanders";
+import { createTransport } from "nodemailer";
 
 export const employeeRouter = Router();
 
@@ -8,10 +10,37 @@ employeeRouter.get("/", async (_req, res: express.Response) => {
 	res.json(employees);
 });
 
-employeeRouter.post(
+employeeRouter.get(
 	"/sendmail/:id",
 	async (req: express.Request, res: express.Response) => {
 		const { id } = req.params;
-		res.send(`info about employee ${id}`);
+		const employees: any = await employeeHandlers.getAllEmployees();
+		const employee = employees.find((m: any) => (m.id = id));
+		const sendTo = "edwardtanguay@gmail.com";
+
+		const mailOptions = {
+			from: `Language Community Site <${process.env.GOOGLE_MAIL_ACCOUNT_USER}@gmail.com>`,
+			to: sendTo,
+			subject: `Information on employee #${employee.id}`,
+			html: `
+Employee #${employee.id}: ${employee.firstName} ${employee.lastName} - ${employee.title}
+`,
+		};
+
+		const transporter = createTransport({
+			service: "gmail",
+			auth: {
+				user: process.env.GOOGLE_MAIL_ACCOUNT_USER,
+				pass: process.env.GOOGLE_MAIL_NODEMAILER_PASSWORD,
+			},
+		});
+
+		transporter.sendMail(mailOptions, (error: any, info: any) => {
+			if (error) {
+				res.send(`ERROR: ${error.message}`);
+			} else {
+				res.send("Email sent: " + info.response);
+			}
+		});
 	}
 );
